f2d7f12971bde3405aeb6ccf7e719f972c3557d5
Diacon Danu <danu.diacon@gmail.com>
2025-10-27 21:59:07 +0200

    Initial

---
+++ Movies.Domain/Movie.cs
@@ -0,0 +1,17 @@
+namespace Movies.Domain;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double Rating { get; set; }
+    public DateTime RealiseDate { get; set; }
+    public List<string> Genres { get; set; }
+    
+    public string PosterUrl { get; set; }
+    public string WatchUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
